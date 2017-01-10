a4eb6b243663b6cf88a6cda50fa169a2fb381718
Student 6 <Student 6@10.8.33.111>
2017-01-10 12:59:25 -0500

    did everything you asked for?

--- SimonAriq/src/partnerCodeInHerePlease/Button.java
+++ SimonAriq/src/partnerCodeInHerePlease/Button.java
@@ -1,67 +1,83 @@
 package partnerCodeInHerePlease;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
+import gui.components.Action;
+import gui.components.Component;
 import packageSimonAriq.ButtonInterfaceAriq;
 
-public class Button implements ButtonInterfaceAriq {
-
+public class Button extends Component implements ButtonInterfaceAriq {
+	/**
+	 * need my partner to have setX(), setY() and setColor()
+	 */
+	private Color c;
+	private int x;
+	private int y;
+	private static int width = 50;
+	private static int height = 50; 
+	
 	public Button() {
-		// TODO Auto-generated constructor stub
+		super(0, 0, width, height);
+		update();
 	}
 
 	@Override
 	public void act() {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public boolean isHovered(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isHovered(int x, int y) {
+		return x>getX() && x<getX()+getWidth() && y > getY() && y<getY()+getHeight();
 	}
 
 	@Override
 	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		return height;
 	}
 
 	@Override
 	public BufferedImage getImage() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
+		return width;
 	}
 
 	@Override
 	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return x;
 	}
 
 	@Override
 	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return y;
 	}
 
 	@Override
 	public boolean isAnimated() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void update(Graphics2D g) {
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		if(c != null) {
+			g.setColor(c);
+			g.fillOval(0, 0, width, height);
+			g.setColor(Color.BLACK);
+			g.drawOval(0, 0, width, height);
+		}
 	}
 
 }
