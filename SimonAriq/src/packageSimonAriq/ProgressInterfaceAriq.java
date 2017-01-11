804643f2524f53654e3a8fd81e3f03fa036cd970
ariqzaman <azaman3835@bths.edu>
2017-01-11 12:04:09 -0500

    Update ProgressInterfaceAriq.java

--- SimonAriq/src/packageSimonAriq/ProgressInterfaceAriq.java
+++ SimonAriq/src/packageSimonAriq/ProgressInterfaceAriq.java
@@ -4,4 +4,11 @@ import gui.components.Visible;
 
 public interface ProgressInterfaceAriq extends Visible {
 
+	void gameOver();
+
+	void setSequenceSize(int size);
+
+	void setRound(int roundNumber);
+
+
 }
